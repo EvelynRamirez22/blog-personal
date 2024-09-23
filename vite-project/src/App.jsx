fb497a4daee1ea0e7cca4a17caed14560e6ea272
Evelyn Ramirez <eveeramirez92@gmail.com>
2024-09-23 15:37:20 -0300

    hicimos el login de github

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -9,38 +9,58 @@ export default function App() {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    const getSession = async () => {
-      const { data, error } = await supabase.auth.getSession();
-      if (error) {
-        console.log(error);
-      } else {
-        setUser(data?.session?.user);
-      }
-    };
+    const session = supabase.auth.getSession();
+    setUser(session?.user);
 
-    getSession();
+    //Switch -> se aplican cuando tenemos que elegir entre ciertas opciones
+    //dependiendo de la expresion que evalua acciona de cierta manera
+    const {
+      data: { subscription }, //estar subscrito a algo significa estar esperando un evento
+    } = supabase.auth.onAuthStateChange((event, session) => {
+      switch (event) {
+        case "SINGED_IN":
+          setUser(session?.user);
+          break;
+        case "SIGNED_OUT":
+          setUser(null);
+          break;
+        default:
+          console.log("caso no estimado");
+      }
+    });
+
+    return () => {
+      subscription.unsubscribe();
+    };
   }, []);
 
   const handleLogin = async () => {
-    const { error, data } = await supabase.auth.signInWithOAuth({
-      provider: "github",
+    //Pide el singInWithOAuth y despues destructuramos solamente el error
+    await supabase.auth.signInWithOAuth({
+      provider: "github", //establece a github para vincuar
     });
-    if (error) {
-      console.log(error);
-    } else {
-      console.log(data);
-    }
+  };
+
+  const handleLogOut = async () => {
+    await supabase.auth.signOut();
   };
 
   return (
     <>
+      {user ? (
+        <div>
+          <h2>Authenticated</h2>
+          <button onClick={handleLogOut}>logout</button>
+        </div>
+      ) : (
+        <button onClick={handleLogin}>login Github</button>
+      )}
       <Header />
-      <button onClick={handleLogin}>Iniciar sesión con Github</button>
       <Post
         titulo={"Título de ejemplo"}
         description={"Descripción foto"}
         link={"IMG-20240903-WA0030.jpg"}
-        parrafo={"Este es el párrafo de la foto"}
+        parrafo={"5to año subiendo la sierra 'Benjamin'."}
       />
       <Footer />
     </>
